7f85df0eb34685fbb0f945242184a23581c64b2a
SangIn Jeong <sanginjeong07@gmail.com>
2023-12-23 19:17:57 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import Box from './component/Box';
-// - 유저는 박스 두 개를 볼 수 있다. (타이틀, 사진, 결과)
-// - 유저는 박스 하단에 가위바위보 버튼을 볼 수 있다.
-// - 버튼 클릭시 클릭한 아이템이 유저박스에 보인다.
-// - 버튼 클릭시 컴퓨터 아이템이 랜덤하게 선택된다.
-// - 승패판단한다.
-// - 지면 빨간색, 이기면 초록색, 비기면 검정색
-// 오빠 화이팅, 항상 응원할게
 
-// state 만들거
-// 버튼에 따라서 가위 바위 보 이미지
-// winner,loser 결과
-// user입장에서 win인지 lose인지 
-// 가위 : 
-// 바위 : 
-// 보 :
 function App() { 
   const choice = {
     'rock' : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVZohJyhyZNNMTYOf7_E16KklxhKnoZQYkwLv1pEgmOfesxr5Akzd7ydV2ad-ECgoRhCM&usqp=CAU',
